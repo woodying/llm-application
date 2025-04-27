4c7a07d739f2517f328dab0e0db017964ea942ff
woodying <wkyungjin1103@gmail.com>
2025-04-27 13:20:53 +0900

    refactor: clean chat.py

--- streamlit/llm.py
+++ streamlit/llm.py
@@ -6,22 +6,28 @@ from langchain import hub
 
 load_dotenv()
 
-llm = ChatOpenAI(model="gpt-4o-mini")
-embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
-vector_store = Chroma(
-    embedding_function = embeddings,
-    persist_directory = "./chroma",
-    collection_name = "tax",
-)
+def get_retriever():
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+    vector_store = Chroma(
+        embedding_function = embeddings,
+        persist_directory = "./chroma",
+        collection_name = "tax",
+    )
+    return vector_store.as_retriever(search_kwargs={'k': 4})
+
+
+def get_llm(model='gpt-4o-mini'):
+    return ChatOpenAI(model=model)
 
 prompt = hub.pull("rlm/rag-prompt")
 
-def get_ai_message(query):
-    qa_chain = RetrievalQA.from_chain_type(
-        llm = llm,
-        retriever = vector_store.as_retriever(),
-        chain_type_kwargs = {"prompt": prompt},
-        return_source_documents=True
-    )
+qa_chain = RetrievalQA.from_chain_type(
+    llm = get_llm(),
+    retriever = get_retriever(),
+    chain_type_kwargs = {"prompt": prompt},
+    return_source_documents=True
+)
 
-    return qa_chain.invoke({"query": query})
+
+def get_ai_message(query):
+    return qa_chain.invoke({"query": query})['result']
