eb3b7ecc7afa69a1b9f3ddffa67925b66f426b3e
woodying <wkyungjin1103@gmail.com>
2025-04-27 13:26:10 +0900

    fix: ai_message

--- streamlit/chat.py
+++ streamlit/chat.py
@@ -18,7 +18,7 @@ if user_question := st.chat_input(placeholder="소득세에 관련된 궁금한 
     st.session_state.message_list.append({"role": "user", "content": user_question})
 
     with st.spinner('답변을 생성하는 중입니다.'):
-        ai_message = get_ai_message(user_question)['result']
+        ai_message = get_ai_message(user_question)
 
         with st.chat_message("ai"):
             st.write(ai_message)
